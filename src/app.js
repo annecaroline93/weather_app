9ffabe7cc03004a24df2d4768fb8804e6d5ade82
“anne.deegan” <anne.deegan@outlook.com>
2022-05-07 15:58:45 -0400

    alteracoes

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
 
+function displayTemperature(response){
+    console.log(response.data);
+    let temperatureElement=document.querySelector("#temperature");
+    let cityElement= document.querySelector("#city");
+    let descriptionElement= document.querySelector("#description");
+    temperatureElement.innerHTML= math.round(response.data.main.temp);
+    cityElement.innerHTML= response.data.name;
+    descriptionElement.innerHTML= response.data.weather[0].description;
+}
+
+let apiKey = "148fea8d023d30747db11939455acef0";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Baltimore&appid=${apiKey}&units=metric';
+
+axios.get(apiUrl).then(displayTemperature);
+
