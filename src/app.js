743c868eba4936f9df3e7d3d10a04c06948dddae
“anne.deegan” <anne.deegan@outlook.com>
2022-05-09 23:09:51 -0400

    novo

--- src/app.js
+++ src/app.js
@@ -1,16 +1,41 @@
 
+function formatDate(timestamp){
+    let date = new Date(timestamp);
+    let hours = date.getHours();
+    if (hours<10){
+        hours=`0${hours}`;
+    }
+    let minutes = date.getMinutes();
+    if (minutes<10){
+        minutes=`0${minutes}`;
+    }
+    let days = ["Sunday", "Monday", "Tuesday","Wednesday","Thursday","Friday","Saturday"];
+    let day = days [date.getDay()];
+    return `${day} ${hours}:${minutes}`;
+
+}
+
+
 function displayTemperature(response){
     console.log(response.data);
     let temperatureElement=document.querySelector("#temperature");
     let cityElement= document.querySelector("#city");
     let descriptionElement= document.querySelector("#description");
-    temperatureElement.innerHTML= math.round(response.data.main.temp);
+    let huminityElement=document.querySelector("#huminity");
+    let windElement=document.querySelector("#wind");
+    let dateElement=document.querySelector("#date");
+    temperatureElement.innerHTML= Math.round(response.data.main.temp);
     cityElement.innerHTML= response.data.name;
     descriptionElement.innerHTML= response.data.weather[0].description;
-}
+    huminityElement.innerHTML= response.data.main.huminity;
+    windElement.innerHTML=response.data.wind.speed;
+    dateElement.innerHTML=formatDate(response.data.dt * 1000);
+
+
+}   
 
 let apiKey = "148fea8d023d30747db11939455acef0";
-let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Baltimore&appid=${apiKey}&units=metric';
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Baltimore&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
 
